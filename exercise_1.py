089a5069079a8851098303c48ec7e66f472cf507
szwanne <103660814+szwanne@users.noreply.github.com>
2025-05-27 23:35:07 +0200

    Created a function that calculates the greatest common divisor

--- exercise_1.py
+++ exercise_1.py
@@ -1,1 +1,12 @@
 #Determining the the greatest common denominator
+def gcf(number_1, number_2):
+    if number_1 > number_2:
+        number_1, number_2 = number_2, number_1
+
+    for i in range (number_1, 0, -1):
+        if number_1 % i == 0 and number_2 % i == 0:
+            return i
+
+number_1 = 260
+number_2 = 80
+print(str(gcf(number_1, number_2)))
